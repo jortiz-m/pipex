45e5959c691fb1caddb12667f278828cd556a3b8
Julia Ortiz Mora <jortiz-m@c3r3s2.42malaga.com>
2024-10-14 13:57:38 +0200

    v2

--- create.c
+++ create.c
@@ -6,24 +6,26 @@
 /*   By: jortiz-m <jortiz-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:08:14 by jortiz-m          #+#    #+#             */
-/*   Updated: 2024/10/14 11:39:03 by jortiz-m         ###   ########.fr       */
+/*   Updated: 2024/10/14 13:57:14 by jortiz-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int *create_pipe(void)
+int	*create_pipe(void)
 {
-    int *pipe_fd = malloc(2 * sizeof(int));
-    if (pipe_fd == NULL)
-    {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    if (pipe(pipe_fd) == -1)
-    {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
-    return (pipe_fd);
+	int	*pipe_fd;
+
+	pipe_fd = malloc(2 * sizeof(int));
+	if (pipe_fd == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	if (pipe(pipe_fd) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+	return (pipe_fd);
 }
